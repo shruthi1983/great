368d1869db28a4baa94e6a5d6b6cc284f5cfc936
shruthi <shruthirmgm@gmail.com>
2022-01-01 12:39:43 +0530

    added table userid to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_133003) do
+ActiveRecord::Schema.define(version: 2022_01_01_065823) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "note"
     t.datetime "created_at", null: false
     t.datetime "completed_at"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
