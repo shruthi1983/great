90197aa8f5a4e6d13a281a0bd0d34c33ebb135a1
shruthi <shruthirmgm@gmail.com>
2022-01-01 14:10:55 +0530

    bootstrapprd login and signup pages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
 	before_action :find_task,only: [:show,:edit,:update,:destroy]
     before_action :authenticate_user!,except: [:index, :show]
- 
+    before_action :correct_user, only: [:edit,:update,:destroy]
 
   def index
   	 @tasks = Task.all
@@ -11,10 +11,12 @@ class TasksController < ApplicationController
   	 @task = Task.find(params[:id])
   end
   def new
-  	 @task = Task.new
+  	#@task = Task.new
+    @task = current_user.tasks.build
   end
   def create
-     @task = Task.new(task_params)
+     #@task = Task.new(task_params)
+     @task = current_user.tasks.build(task_params)
   	   if @task.save
   	 	  redirect_to @task
       else
@@ -39,8 +41,12 @@ class TasksController < ApplicationController
   def find_task
    	 @task = Task.find(params[:id])
   end
+  def correct_user
+    @tasks =current_user.tasks.find_by(id: params[:id])
+    redirect_to tasks_path,notice:"not authorize to edit" if @task.nil?
+  end
  private
   def task_params
-     params.require(:task).permit(:note,:created_at,:completed_at)
+     params.require(:task).permit(:note,:created_at,:completed_at,:user_id)
   end
 end
